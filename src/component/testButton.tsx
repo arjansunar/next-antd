910c99cae205ac6dc5e4a89494a1e59b548c1d73
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-05-27 13:06:21 +0545

    Use simple button with antd

---
+++ src/component/testButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "antd";
+import React from "react";
+
+type Props = {};
+
+const TestButton = (props: Props) => {
+  return <Button>testButton</Button>;
+};
+
+export { TestButton };
